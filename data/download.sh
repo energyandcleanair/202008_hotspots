7eaab0575a3115c3b8b8b7eb937b241a04fb9f7d
Hubert Thieriot <hubert@energyandcleanair.org>
2020-09-03 14:02:07 +0200

    -moved all analysis to 202008_hotspots_analysis repo

--- data/download.sh
+++
@@ -1,1 +0,0 @@
-wget --load-cookies ~/.urs_cookies --save-cookies ~/.urs_cookies --auth-no-challenge=on --keep-session-cookies --content-disposition -i SSW_download_2020-08-13T17_03_38_1262_s5HdqB4R.inp.txt
